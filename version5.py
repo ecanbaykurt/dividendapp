ebec3ceba9f4b79d88fb94ecd9da76f1c7ca4b8e
Emre Can Baykurt <ebaykurt@bu.edu>
2025-04-29 12:39:43 -0400

    version5.py

--- version5.py
+++ version5.py
@@ -330,8 +330,8 @@ def hidden_competitor_neural_map():
                 zaxis=dict(backgroundcolor='white', gridcolor='lightgrey'),
                 bgcolor='white'
             ),
-            paper_bgcolor='white',
-            plot_bgcolor='white',
+            paper_bgcolor='dark',
+            plot_bgcolor='dark',
             font=dict(color='black', size=12),
             coloraxis_colorbar=dict(
                 title="Density",
